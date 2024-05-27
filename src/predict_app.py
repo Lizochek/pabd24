d3b7ac3f81c8f035bd8e9d94b1c2587ca22eaa1a
Elizaveta Kulichenko <Elicoolich@yandex.ru>
2024-05-27 20:55:43 +0300

    added utils.py

--- src/predict_app.py
+++ src/predict_app.py
@@ -37,7 +37,8 @@ def predict(in_data: dict) -> int:
     :rtype: int
     """
     area = float(in_data['area'])
-    price = model.predict([[area]])
+    #price = model.predict([[area]])
+    price = predict_cpu_bounded(area, 10000000)
     return int(price)
 
 
