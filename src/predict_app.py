ecd11b986b96566bdc4bb5812225bd94d645db84
Elizaveta Kulichenko <Elicoolich@yandex.ru>
2024-05-27 20:18:12 +0300

    added utils.py

--- src/predict_app.py
+++ src/predict_app.py
@@ -1,11 +1,32 @@
 """House price prediction service"""
 import os
-
-from flask import Flask, request, send_from_directory
+from dotenv import dotenv_values
+from flask import Flask, request, url_for
 from flask_cors import CORS
+from joblib import load
+from flask_httpauth import HTTPTokenAuth
+from flask import send_from_directory
+from utils import predict_io_bounded, predict_cpu_bounded, predict_cpu_multithread
+
+MODEL_SAVE_PATH = 'models/linear_regression_v01.joblib'
 
 app = Flask(__name__)
 CORS(app)
+
+config = dotenv_values(".env")
+auth = HTTPTokenAuth(scheme='Bearer')
+
+tokens = {
+    config['APP_TOKEN']: "user1",
+}
+
+model = load(MODEL_SAVE_PATH)
+
+
+@auth.verify_token
+def verify_token(token):
+    if token in tokens:
+        return tokens[token]
 
 
 def predict(in_data: dict) -> int:
@@ -16,21 +37,34 @@ def predict(in_data: dict) -> int:
     :rtype: int
     """
     area = float(in_data['area'])
-    AVG_PRICE = 200_000                 # RUB / m2
-    return int(area * AVG_PRICE)
+    price = model.predict([[area]])
+    return int(price)
+
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 @app.route("/")
 def home():
-    return '<h1>Housing price service.</h1> Use /predict endpoint'
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/x-icon')
+    return """
+    <html>
+    <head>
+    <link rel="shortcut icon" href="/favicon.ico">
+    </head>
+    <body>
+    <h1>Housing price service.</h1> Use /predict endpoint
+    </body>
+    </html>
+    """
 
 
 @app.route("/predict", methods=['POST'])
+@auth.login_required
 def predict_web_serve():
     """Dummy service"""
     in_data = request.get_json()
