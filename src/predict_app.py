c23e4f81dd46942f7f279f1589b4fbb5422db29c
Elizaveta Kulichenko <Elicoolich@yandex.ru>
2024-06-10 21:24:47 +0300

    predict_app for_new_model

--- src/predict_app.py
+++ src/predict_app.py
@@ -37,9 +37,16 @@ def predict(in_data: dict) -> int:
     :rtype: int
     """
     area = float(in_data['total_meters'])
-    #price = model.predict([[area]])
+    floor = int(in_data['floor'])
+    floors_count = int(in_data['floors_count'])
+    is_first = (floor == 1)
+    is_last = (floor == floors_count)
+    price = model.predict([[area,
+                            is_first,
+                            is_last,
+                            floors_count]])
     #price = predict_cpu_bounded(area, 7_000_000)
-    price = predict_cpu_multithread(area, 7_000_000)
+    #price = predict_cpu_multithread(area, 7_000_000)
     return int(price)
 
 @app.route('/favicon.ico')
