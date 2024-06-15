2bd2faaf0307bafe1c216cd79bfeb2c2b3724600
Elizaveta Kulichenko <Elicoolich@yandex.ru>
2024-06-15 13:39:39 +0300

    change

--- src/predict_app.py
+++ src/predict_app.py
@@ -50,9 +50,9 @@ def predict(in_data: dict) -> int:
     #                         rooms_count
     #                         ]])
     # price = model.predict([[area]])
-    price = predict_io_bounded(area)
+    # price = predict_io_bounded(area)
 
-    # price = predict_cpu_bounded(area, 7_000_000)
+    price = predict_cpu_bounded(area, 7_000_000)
     # price = predict_cpu_multithread(area, 7_000_000)
     return int(price)
 
