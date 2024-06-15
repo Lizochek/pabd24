f1abc62af99341f6ce9a0f78cf98c1bbacd70351
Elizaveta Kulichenko <Elicoolich@yandex.ru>
2024-06-15 14:13:48 +0300

    change

--- test/test_parallel.py
+++ test/test_parallel.py
@@ -3,9 +3,14 @@ from multiprocessing import Pool
 import requests
 from dotenv import dotenv_values
 
+proxies = {
+    "http:" "",
+    "https:" "",
+}
 config = dotenv_values(".env")
 endpoint = 'http://192.144.14.11:5000/predict'
 HEADERS = {"Authorization": f"Bearer {config['APP_TOKEN']}"}
+
 
 def do_request(area: int) -> str:
     data = {'total_meters': area}
@@ -22,6 +27,7 @@ def do_request(area: int) -> str:
 def test_10():
     with Pool(10) as p:
         print(*p.map(do_request, range(10, 110, 10)))
+
 
 if __name__ == '__main__':
     test_10()
