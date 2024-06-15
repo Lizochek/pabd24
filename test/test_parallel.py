4540d072d6b7277b787b050d21c292c711beb502
Elizaveta Kulichenko <Elicoolich@yandex.ru>
2024-06-15 14:55:27 +0300

    change

--- test/test_parallel.py
+++ test/test_parallel.py
@@ -3,38 +3,32 @@ from multiprocessing import Pool
 import requests
 from dotenv import dotenv_values
 
-proxies = {
-    "http:" "",
-    "https:" "",
-}
-
 config = dotenv_values(".env")
-endpoint = 'http://192.144.14.11:5000/predict'
+endpoint = 'hhttp://192.144.14.11:5000'
 HEADERS = {"Authorization": f"Bearer {config['APP_TOKEN']}"}
 
 
 def do_request(area: int) -> str:
+    # data = {'total_meters': area,
+    #             'rooms_count': 1,
+    #             'floor': 1,
+    #             'floors_count': 1,
+    #             'county_short': "ЦАО",
+    #             'object_type': "Новостройка"}
     data = {'total_meters': area}
     t0 = time.time()
-    try:
-        resp = requests.post(
-            endpoint,
-            json=data,
-            headers=HEADERS
-        )
-        resp.raise_for_status()  # Проверяем, что запрос успешен
-        response_text = resp.text
-    except requests.exceptions.RequestException as e:
-        response_text = f"Error: {e}"
+    resp = requests.post(
+        endpoint,
+        json=data,
+        headers=HEADERS
+    ).text
     t = time.time() - t0
-    return f'Waited {t:0.2f} sec {response_text}'
+    return f'Waited {t:0.2f} sec ' + resp
 
 
 def test_10():
     with Pool(10) as p:
-        results = p.map(do_request, range(10, 110, 10))
-        for result in results:
-            print(result)
+        print(*p.map(do_request, range(10, 110, 10)))
 
 
 if __name__ == '__main__':
