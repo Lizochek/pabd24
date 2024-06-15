040257ae8a26f5f3f657da7d9a6bc1335c9f3694
Elizaveta Kulichenko <Elicoolich@yandex.ru>
2024-06-15 14:37:28 +0300

    change

--- test/test_parallel.py
+++ test/test_parallel.py
@@ -3,31 +3,38 @@ from multiprocessing import Pool
 import requests
 from dotenv import dotenv_values
 
-# proxies = {
-#     "http:" "",
-#     "https:" "",
-# }
+proxies = {
+    "http:" "",
+    "https:" "",
+}
 
 config = dotenv_values(".env")
-endpoint = 'http://192.144.14.11:5000/predict'
+endpoint = 'http://127.0.0.1:5000/predict'
 HEADERS = {"Authorization": f"Bearer {config['APP_TOKEN']}"}
 
 
 def do_request(area: int) -> str:
     data = {'total_meters': area}
     t0 = time.time()
-    resp = requests.post(
-        endpoint,
-        json=data,
-        headers=HEADERS
-    ).text
+    try:
+        resp = requests.post(
+            endpoint,
+            json=data,
+            headers=HEADERS
+        )
+        resp.raise_for_status()  # Проверяем, что запрос успешен
+        response_text = resp.text
+    except requests.exceptions.RequestException as e:
+        response_text = f"Error: {e}"
     t = time.time() - t0
-    return f'Waited {t:0.2f} sec ' + resp
+    return f'Waited {t:0.2f} sec {response_text}'
 
 
 def test_10():
     with Pool(10) as p:
-        print(*p.map(do_request, range(10, 110, 10)))
+        results = p.map(do_request, range(10, 110, 10))
+        for result in results:
+            print(result)
 
 
 if __name__ == '__main__':
